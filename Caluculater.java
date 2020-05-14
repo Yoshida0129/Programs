313eca9fc0b4be28262b53573977ee0345c01782
Tatsuya Yoshida <alpacagame0129@gmail.com>
2020-05-14 23:36:36 +0900

    Refector: ひらがなカタカナ対応、

--- Caluculater.java
+++ Caluculater.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 import java.util.regex.*;
+import java.util.function.Function;
 import java.lang.String;
 
-public class App {
+public class Caluculater {
     /**
      * 計算記号の実行
      * @param  int    a       計算数値
@@ -20,23 +21,25 @@ public class App {
 				return a * b;
 			case "/":
 				return a / b;
-			default:
+            default:
+                System.err.println("符号が不正です");
 				return -1;
 		}
     }
 
     /**
-     * 正規表現にマッチしたパラメータを返す（削除予定）
+     * 正規表現にマッチしたパラメータを返す
      * @param  String Regex   正規表現
      * @param  String formula 文字列の式　例："3+2"
      * @return String         正規表現のパラメータを返す
      */
     private static String getStrByRegex (String Regex, String formula) {
+        if (!checkError(formula)){
+            System.err.println("無限エラー");
+        }
         Pattern p = Pattern.compile(Regex);
         Matcher matcher = p.matcher(formula);
-        if(matcher.find()){
-            return matcher.group();
-        }
+        if(matcher.find()) return matcher.group();
         String res = matcher.group();
         return res;
     }
@@ -59,11 +62,10 @@ public class App {
      * @return               計算結果
      */
     private static int berekeningAndReplace(String formula){
-        String symbol = "*";
+        String symbol = "+";
         int a = Integer.parseInt(getStrByRegex("^(\\-)?[0-9]+", formula));
-        try{
-            symbol = getStrByRegex("(\\+|\\*|\\/)", formula);
-        }catch(Exception Err){}
+        try { symbol = getStrByRegex("(\\+|\\*|\\/)", formula);}
+        catch (Exception Err) {}
 		int b = Integer.parseInt(getStrByRegex("(\\-)?[0-9]+$", formula));
 		return berekening(a, b, symbol);
     }
@@ -72,57 +74,80 @@ public class App {
      * 計算処理
      * @param input String 入力文字列
      */
-    public static void run(String input){
+    private static void run(String input){
         final String BRACKET_REGEX = "\\([^\\(\\)]*(-)?[0-9]+((\\+|\\-|\\*|\\/)?[0-9]+)*\\)";
         final String BRACKET_ONE_NUM_REGEX = "\\((\\-)?[0-9]+\\)";
         final String FIRST_BEREKENING_REGEX =  "(\\-)?[0-9]+((\\*|\\/)|(\\-))+[0-9]+"; // 乗算割算抽出
         final String SECOND_BEREKENING_REGEX = "(\\-)?[0-9]+(\\+|\\-)+[0-9]+"; // 乗算割算抽出
 
-        final String ERROR_INFI = "0/[0-9]?";
+        Function<String, String> deleteBracket = (str) -> str.replaceAll("\\(((\\-)?[0-9]+((\\+|\\-|\\*|\\/)+[0-9]+)*)\\)", "$1");
 
-        String inputString = input.replaceAll(" ", "");
-        while(getBooByRegex(ERROR_INFI, inputString)){
-            System.out.println("無限大の結果が出る恐れのあるものは入力しれください");
-            inputString = new Scanner(System.in).nextLine().replaceAll(" ", "");
-        }
-
-        inputString = inputString.replaceAll("((\\)|[0-9])+)+\\(", "$1*(");
+        String inputString = input.replaceAll("((\\)|[0-9])+)+\\(", "$1*(");
         while(getBooByRegex(BRACKET_REGEX, inputString) || getBooByRegex(BRACKET_ONE_NUM_REGEX, inputString) ) {
             String tempBracket = getStrByRegex(BRACKET_REGEX, inputString);
-            String temp = tempBracket.replace("(", "").replace(")", "");
-        	while(getBooByRegex(FIRST_BEREKENING_REGEX, temp)) {
+            String temp = deleteBracket.apply(tempBracket);
+            while(getBooByRegex(FIRST_BEREKENING_REGEX, temp)) {
                 String result = String.valueOf(berekeningAndReplace(temp));
                 inputString = inputString.replace(tempBracket, result);
                 try {
                     tempBracket = getStrByRegex(BRACKET_REGEX, inputString);
-                    temp = tempBracket.replace("(", "").replace(")", "");
+                    temp = deleteBracket.apply(tempBracket);
                 } catch (Exception Err) {break;}
             }
-        	while(getBooByRegex(SECOND_BEREKENING_REGEX, temp)) {
+            while(getBooByRegex(SECOND_BEREKENING_REGEX, temp)) {
                 String result = String.valueOf(berekeningAndReplace(temp));
                 inputString = inputString.replace(tempBracket, result);
                 try {
                     tempBracket = getStrByRegex(BRACKET_REGEX, inputString);
-                    temp = tempBracket.replace("(", "").replace(")", "");
+                    temp = deleteBracket.apply(tempBracket);
                 } catch (Exception Err) {break;}
             }
         }
 
         while(getBooByRegex(FIRST_BEREKENING_REGEX, inputString)) {
-    		String tempFirst = getStrByRegex(FIRST_BEREKENING_REGEX, inputString);
+            String tempFirst = getStrByRegex(FIRST_BEREKENING_REGEX, inputString);
             inputString = inputString.replace(tempFirst, String.valueOf(berekeningAndReplace(tempFirst)));
         }
 
         while(getBooByRegex(SECOND_BEREKENING_REGEX, inputString)) {
-    		String tempSecond = getStrByRegex(SECOND_BEREKENING_REGEX, inputString);
+            String tempSecond = getStrByRegex(SECOND_BEREKENING_REGEX, inputString);
             inputString = inputString.replace(tempSecond, String.valueOf(berekeningAndReplace(tempSecond)));
         }
         System.out.println(inputString);
     }
 
+    private static Boolean checkError(String inputString){
+        final String ERROR_INFI = "0/[0-9]?";
+        final String ERROR_JPN = "[ぁ-んー]|[ァ-ケー]|[ｦ-ﾟ]";
+        final String ERROR_STR = "(!|@|#|\\$|%|\\^|&|:|_|~|\\{|\\}|\\|>|<|\\?|=|\\`|\\[|\\]|;|\\'|,|\\/|\\.)+";
+
+        if(getBooByRegex(ERROR_INFI, inputString)){
+            System.out.println("無限大の結果が出る恐れのあるものは入力ないでください");
+            return false;
+        }
+        if(getBooByRegex(ERROR_JPN, inputString)){
+            System.out.println("日本語文字を使用しないでください");
+            return false;
+        }
+        if(getBooByRegex(ERROR_STR, inputString)){
+            System.out.println("不明な文字が存在しています。");
+            return false;
+        }
+        return true;
+    }
+
+    private static String input(){
+        while(true){
+            System.out.println("input caluculater");
+            String inputString = new Scanner(System.in).nextLine().replaceAll(" ", "");
+            if(checkError(inputString)){
+                return inputString;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-    	System.out.println("input caluculater");
-        String inputString = new Scanner(System.in).nextLine();
+        String inputString = input();
         run(inputString);
     }
 }
